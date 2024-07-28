9fd6fe3e5696c9816ad43345bd19443987a0c61f
krngill97 <gillkrnp@gmail.com>
2024-07-28 21:57:04 +0000

    datatypes

---
+++ basic_1/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";  //treat all JS code as newer version
+
+//alert(3+3)// we are usingnodejs,not browser that is why error
+
+// tc39.es or mdn website for standards and documentaion
+
+let name="pepe"
+let age= 12
+let login=false
+let state;
+let temp=null
+
+//datatypes: primitive datatypes
+
+ // number=> 2 to power 53
+ // bigint use for bigger value
+ // string
+ // boolean=false/true
+ // null =standalone value and outpout is object and null is type
+ // undefined => undefined output
+ // symbol = unique, used in react for uniqueness
+
+ //objects:
+
+ console.log(typeof age, false , name,)
+
+ console.log( typeof null)//object
+ console.log( typeof undefined)//undefined
+
+
