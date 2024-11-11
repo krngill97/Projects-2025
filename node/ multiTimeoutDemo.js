4b141338e2bf01761cecc5a2480b0f5c32a9f8f7
krngill97 <gillkrnp@gmail.com>
2024-11-11 06:24:24 +0000

    efad

---
+++ node/ multiTimeoutDemo.js
@@ -0,0 +1,12 @@
+console.log('task 1:start');
+
+setTimeout(()=>{
+    console.log('task 2: imeout dter 3 seconds');
+},3000);
+
+setTimeout(()=>{
+    console.log('task 3: timeout after 1 second');
+
+},1000);
+
+console.log('tasl 4: end');
